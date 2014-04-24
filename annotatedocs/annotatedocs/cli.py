c706123fc492224b98760ddea10db605661b802c
Gregor Müllegger <gregor@muellegger.de>
2014-04-24 16:08:21 +0000

    Building sphinx project (doctrees) and allow recreation of environment.
    git-svn-id: https://scm.ds-lab.org:8443/scm/svn/abschlussarbeiten/muellegger@17 4cdc706f-4501-0010-b6e1-057a65fcbcb9

--- annotatedocs/annotatedocs/cli.py
+++ annotatedocs/annotatedocs/cli.py
@@ -4,6 +4,7 @@ from .readthedocs import get_project
 
 @click.command()
 @click.argument('project', help='A read the docs project name.')
-def main(project):
+@click.option('--recreate/--no-recreate', default=False, help='Recreate all cached files.')
+def main(project, recreate=False):
     project = get_project(project)
-    project.setup()
+    project.setup(recreate=recreate)
--- annotatedocs/annotatedocs/readthedocs.py
+++ annotatedocs/annotatedocs/readthedocs.py
@@ -2,6 +2,7 @@ import os
 
 from logbook import Logger
 import sh
+import shutil
 import slumber
 
 from .utils import cd
@@ -16,17 +17,28 @@ api = slumber.API(base_url='http://readthedocs.org/api/v1/')
 class Project(object):
     dependencies = ['Sphinx']
 
-    def __init__(self, slug, repository, file_suffix, install_python_package, pip_requirements_file):
+    def __init__(self, slug, repository, file_suffix, install_python_package,
+            pip_requirements_file):
         self.slug = slug
         self.repository_url = repository
         self.file_suffix = file_suffix
         self.install_python_package = install_python_package
         self.pip_requirements_file = pip_requirements_file
 
-        self.repository_dir = '/tmp/annotatedocs/{}/repo'.format(slug)
-        self.virtualenv_dir = '/tmp/annotatedocs/{}/venv'.format(slug)
+        self.base_dir = os.path.join('/tmp', 'annotatedocs', slug)
+        self.repository_dir = os.path.join(self.base_dir, 'repo')
+        self.virtualenv_dir = os.path.join(self.base_dir, 'venv')
+        self.docs_dir = os.path.join(self.repository_dir, 'docs')
+        self.build_dir = os.path.join(self.base_dir, 'build')
+        self.doctrees_dir = os.path.join(self.build_dir, 'doctrees')
+        self.build_html_dir = os.path.join(self.build_dir, 'html')
 
-    def checkout_repository(self):
+    def setup_repository(self):
+        exists = os.path.exists(self.repository_dir)
+        if exists:
+            log.debug('Using existing checkout: {}'.format(self.repository_dir))
+            return
+
         log.info('Checking out {} -> {}'.format(
             self.repository_url,
             self.repository_dir))
@@ -34,64 +46,69 @@ class Project(object):
             os.makedirs(self.repository_dir)
         sh.git.clone(self.repository_url, self.repository_dir)
 
-    def update_repository(self):
-        log.info('Updating repository in {}'.format(self.repository_dir))
-        with cd(self.repository_dir):
-            output = sh.git.pull()
-            log.debug('git pull: {}'.format(output))
-
-    def setup_repository(self):
-        if not os.path.exists(os.path.join(self.repository_dir, '.git')):
-            self.checkout_repository()
-        else:
-            log.debug('Directory already exists, assuming it contains the right checkout: {}'.format(self.repository_dir))
-            self.update_repository()
-
     def get_virtualenv_command(self, executable):
         return sh.Command(os.path.join(self.virtualenv_dir, 'bin', executable))
 
-    def setup_virtualenv(self):
-        log.info('Setting up virtualenv in {}'.format(self.virtualenv_dir))
-        if not os.path.exists(self.virtualenv_dir):
+    def setup_virtualenv(self, recreate=False):
+        exists = os.path.exists(self.virtualenv_dir)
+        if not exists:
+            log.info('Setting up virtualenv in {}'.format(self.virtualenv_dir))
             os.makedirs(self.virtualenv_dir)
 
-        # Setup virtualenv.
-        sh.virtualenv(self.virtualenv_dir)
+            # Setup virtualenv.
+            sh.virtualenv(self.virtualenv_dir)
 
-        pip = self.get_virtualenv_command('pip')
+            pip = self.get_virtualenv_command('pip')
 
-        # Install Sphinx.
-        for output in pip.install('--upgrade', *self.dependencies, _iter=True):
-            log.debug('pip install: {}'.format(output))
+            # Install Sphinx.
+            for output in pip.install('--upgrade', *self.dependencies, _iter=True):
+                log.debug('pip install: {}'.format(output))
 
-        # Install python package if needed.
-        if self.install_python_package:
-            python = self.get_virtualenv_command('python')
-            with cd(self.repository_dir):
-                log.info('Installing python package')
-                python('setup.py', 'install')
+            # Install python package if needed.
+            if self.install_python_package:
+                python = self.get_virtualenv_command('python')
+                with cd(self.repository_dir):
+                    log.info('Installing python package')
+                    python('setup.py', 'install')
 
-        # Install project's dependencies if there are any.
-        if self.pip_requirements_file:
-            requirements_file = os.path.join(
-                self.repository_dir,
-                self.pip_requirements_file)
-            log.info('Installing python dependencies from {}'.format(requirements_file))
-            pip.install(r=requirements_file)
+            # Install project's dependencies if there are any.
+            if self.pip_requirements_file:
+                requirements_file = os.path.join(
+                    self.repository_dir,
+                    self.pip_requirements_file)
+                log.info('Installing python dependencies from {}'.format(requirements_file))
+                pip.install(r=requirements_file)
+        else:
+            log.debug('Using existing virtualenv: {}'.format(self.virtualenv_dir))
 
     def build_doctrees(self):
-        pass
+        exists = os.path.exists(self.doctrees_dir)
+        if exists:
+            log.debug('Using existing doctrees: {}'.format(self.doctrees_dir))
+            return
 
-    def setup(self):
+        log.info('Building doctrees.')
+        sphinx_build = self.get_virtualenv_command('sphinx-build')
+        stdout = sphinx_build(
+            '-b', 'html',
+            '-d', self.doctrees_dir,
+            self.docs_dir,
+            self.build_html_dir,
+            _iter=True)
+        for line in stdout:
+            log.debug('sphinx-build: {}'.format(line))
+
+    def setup(self, recreate=False):
         '''
         Prepare all the steps that are needed before we can attempt a build.
         '''
 
+        if recreate:
+            shutil.rmtree(self.base_dir)
+
         self.setup_repository()
         self.setup_virtualenv()
-
-    def build(self):
-        pass
+        self.build_doctrees()
 
 
 def get_project(slug):
