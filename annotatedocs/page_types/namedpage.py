21e28fd9a70a9a8da23c1a60a9eacfc1887aa606
Gregor Müllegger <gregor@muellegger.de>
2014-08-23 19:42:51 +0200

    Integrating NamedPage page type into the annotatedocs core.

---
+++ annotatedocs/page_types/namedpage.py
@@ -0,0 +1,29 @@
+from annotatedocs.page_types import PageType
+
+
+class NamedPage(PageType):
+    """
+    This page type always matches to the document name given in the init
+    method. It's useful to explicitly applying a set of checks onto a specific
+    document.
+
+    Example::
+
+        bundle = Bundle(
+            NamedPage('installation_guide', [InstallationGuideCheck])
+
+    This will apply the ``InstallationGuideCheck`` to the
+    ``installtion_guide.rst`` file.
+    """
+
+    def __init__(self, document_name, checks):
+        self.document_name = document_name
+        self.checks = checks
+        super(NamedPage, self).__init__()
+
+    @property
+    def name(self):
+        return self.document_name
+
+    def match(self, document):
+        return int(document.name == self.document_name)
--- tests/sampledocs/docsbundle.py
+++ tests/sampledocs/docsbundle.py
@@ -1,19 +1,9 @@
 from annotatedocs.bundle import Bundle
 from annotatedocs.checks import LongSection, PassiveVoice
-from annotatedocs.page_types import PageType, BasicPage, InstallationGuide
-
-
-def named_page(document_name, *additional_checks):
-    class NamedPage(PageType):
-        checks = PageType.checks + list(additional_checks)
-
-        def match(self, document):
-            return document.name == document_name
-
-    return NamedPage
+from annotatedocs.page_types import NamedPage, InstallationGuide
 
 
 bundle = Bundle(
     InstallationGuide,
-    named_page('basicpage/longsections', LongSection),
-    named_page('basicpage/passivevoice', PassiveVoice))
+    NamedPage('basicpage/longsections', [LongSection]),
+    NamedPage('basicpage/passivevoice', [PassiveVoice]))
